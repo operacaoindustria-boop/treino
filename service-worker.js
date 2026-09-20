4df98eccebf9d0dbe9864edd6cfa7c13036f9284
italopw <ianitalo832@gmail.com>
2026-09-20 15:47:10 -0400

    Configura Firebase Cloud Messaging

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,30 @@
 const notificationIcon = 'https://cdn-icons-png.flaticon.com/512/2964/2964514.png';
 
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.14.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyC56zt9xGfusTYmp1XtEGneig1uhf9ZR7Q',
+  authDomain: 'farmtell-beef-analytics.firebaseapp.com',
+  projectId: 'farmtell-beef-analytics',
+  storageBucket: 'farmtell-beef-analytics.firebasestorage.app',
+  messagingSenderId: '686184908638',
+  appId: '1:686184908638:web:99b39d498a5f8d38c8b1cf'
+});
+
+const firebaseMessaging = firebase.messaging();
+firebaseMessaging.onBackgroundMessage(payload => {
+  const notification = payload.notification || payload.data || {};
+  self.registration.showNotification(notification.title || 'Titan OS', {
+    body: notification.body || 'Novo alerta',
+    icon: notification.icon || notificationIcon,
+    badge: notificationIcon,
+    vibrate: [300, 100, 300],
+    requireInteraction: true,
+    data: { url: notification.url || './' }
+  });
+});
+
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
